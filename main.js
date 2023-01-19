86982d88bad5288ea61f541ea7e2109e24a5cd9e
Oliwier Posiakow <oliwierposiek@gmail.com>
2023-01-19 19:59:16 +0100

    Created working Searchbar
    Movies are displaying after searching for a title, fixed some css in 'movie' class

--- main.js
+++ main.js
@@ -1,6 +1,43 @@
 //44d65228 API Key
 //Base: http://www.omdbapi.com/
 //Endpoints: ?apikey=44d65228&s=???
-fetch('http://www.omdbapi.com/?apikey=44d65228&s=star_wars')
-    .then(res => res.json())
-    .then(data => console.log(data))
+const contentEl = document.querySelector('.content')
+
+
+
+document.querySelector('.search').addEventListener('click', async () =>{
+    const inputVal = document.getElementById('input-el')
+    const res = await fetch(`http://www.omdbapi.com/?apikey=44d65228&s=${inputVal.value}`)
+    const data = await res.json()
+    let filmHTML = ''
+
+    for(let film of data.Search){
+        const title = film.Title
+        console.log(title)
+        const res = await fetch(`http://www.omdbapi.com/?apikey=44d65228&t=${title}`)
+        const data = await res.json()
+        console.log(data.Poster)
+
+        let poster = ''
+        if(data.Poster === 'N/A'){
+            poster = 'images/placeholder.jpg'
+        }
+        else{
+            poster = `${data.Poster}`
+        }
+
+        filmHTML += `
+            <div class="movie">
+                <img class="poster" src='${poster}'>
+                <h5 class="title">${data.Title}</h5>
+                <i class="fa-solid fa-star"><p class="rate">${data.imdbRating}</p></i>
+                <p class="watchtime">${data.Runtime}</p>
+                <p class="genre">${data.Genre}</p>
+                <button class="film-btn" id="add-btn"><i class="fa-solid fa-circle-plus"></i>Watchlist</button>
+                <p class="plot">${data.Plot}</p>
+            </div>
+        `
+    }
+    inputVal.value = ''
+    contentEl.innerHTML = filmHTML   
+})
