90f1b49141e2e7309933cce321e2a80228bdf225
Oliwier Posiakow <oliwierposiek@gmail.com>
2023-01-23 21:41:24 +0100

    Big changes
    Added watchlist tab, updated css and js, added feature when there is no movies found text is displayed.

--- main.js
+++ main.js
@@ -2,45 +2,52 @@
 //Base: http://www.omdbapi.com/
 //Endpoints: ?apikey=44d65228&s=???
 const contentEl = document.querySelector('.content')
-
-
+const defDiv = document.querySelector('.def-ph')
+let loadedMovies = []
 
 document.querySelector('.search').addEventListener('click', async () =>{
-    const inputVal = document.getElementById('input-el')
-    const res = await fetch(`http://www.omdbapi.com/?apikey=44d65228&s=${inputVal.value}`)
-    const data = await res.json()
-    let filmHTML = ''
-
-    for(let film of data.Search){
-        const title = film.Title
-        console.log(title)
-        const res = await fetch(`http://www.omdbapi.com/?apikey=44d65228&t=${title}`)
+    try{
+        loadedMovies = []
+        const inputVal = document.getElementById('input-el')
+        const res = await fetch(`http://www.omdbapi.com/?apikey=44d65228&s=${inputVal.value}`)
         const data = await res.json()
-
-        let poster = ''
-        if(data.Poster === 'N/A' || data.Poster === undefined){
-            poster = 'images/placeholder.jpg'
+        let filmHTML = ''
+        for(let film of data.Search){
+            const title = film.Title
+            const res = await fetch(`http://www.omdbapi.com/?apikey=44d65228&t=${title}`)
+            const data = await res.json()
+            loadedMovies.push(data)
+            console.log(data)
+            let poster = ''
+            if(data.Poster === 'N/A' || data.Poster === undefined){
+                poster = 'images/placeholder.jpg'
+            }
+            else{
+                poster = `${data.Poster}`
+            }
+            if(data.Title === undefined){
+                filmHTML += ``
+            }
+            else{
+                filmHTML += `
+                <div class="movie">
+                    <img class="poster" src='${poster}'>
+                    <h5 class="title">${data.Title}</h5>
+                    <i class="fa-solid fa-star"><p class="rate">${data.imdbRating}</p></i>
+                    <p class="watchtime">${data.Runtime}</p>
+                    <p class="genre">${data.Genre}</p>
+                    <button class="film-btn" id="add-btn"><i class="fa-solid fa-circle-plus"></i>Watchlist</button>
+                    <p class="plot">${data.Plot}</p>
+                </div>
+            `
+            }
         }
-        else{
-            poster = `${data.Poster}`
-        }
-        if(data.Title === undefined){
-            filmHTML += ``
-        }
-        else{
-            filmHTML += `
-            <div class="movie">
-                <img class="poster" src='${poster}'>
-                <h5 class="title">${data.Title}</h5>
-                <i class="fa-solid fa-star"><p class="rate">${data.imdbRating}</p></i>
-                <p class="watchtime">${data.Runtime}</p>
-                <p class="genre">${data.Genre}</p>
-                <button class="film-btn" id="add-btn"><i class="fa-solid fa-circle-plus"></i>Watchlist</button>
-                <p class="plot">${data.Plot}</p>
-            </div>
-        `
-        }
+        inputVal.value = ''
+        contentEl.innerHTML = filmHTML  
+        console.log(loadedMovies)     
     }
-    inputVal.value = ''
-    contentEl.innerHTML = filmHTML   
+    catch (e){
+        contentEl.innerHTML = `<div class='no-results'><p>Unable to find what you’re looking for. Please try another search.</p></div>`
+    }
+
 })
---
+++ watchlistData.js
@@ -0,0 +1,3 @@
+const watchlist = []
+
+export default watchlist
